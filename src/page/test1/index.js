854fc55424ab0fb8f7c82d0cb6411d0ce0e48382
mianmalife <253590433@qq.com>
2021-06-29 23:38:45 +0800

    add:antd-dayjs-webpack-plugin

--- src/page/test1/index.js
+++ src/page/test1/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { DatePicker, message } from 'antd'
+import dayjs from 'dayjs'
 import 'antd/dist/antd.css'
+import 'dayjs/locale/zh-cn'
 import './index.less'
 import smile from '@/assets/image/a.jpg'
 import { getUserInfo } from '@/request/api'
+dayjs.locale('zh-cn')
 export default class App extends React.Component {
     componentDidMount() {
         getUserInfo({ a: 1, b: 2 }).then(res => {
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,15 +2,30 @@ const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const AntdDayjsWebpackPlugin = require('antd-dayjs-webpack-plugin')
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 module.exports = merge(common, {
     mode: 'production',
     performance: {
         hints: false
     },
+    // optimization: {
+    //     splitChunks: {
+    //         cacheGroups: {
+    //             vendor: {
+    //                 test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+    //                 name: 'vendor',
+    //                 chunks: 'all',
+    //             }
+    //         }
+    //     }
+    // },
     plugins: [
         new MiniCssExtractPlugin({
             filename: '[name]/[name]-[hash:12].css'
         }),
-        new OptimizeCssAssetsPlugin()
+        new OptimizeCssAssetsPlugin(),
+        new AntdDayjsWebpackPlugin(),
+        // new BundleAnalyzerPlugin()
     ]
 })
