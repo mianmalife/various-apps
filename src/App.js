3fbddcab9ade2b4b1c2439e703acd5f549d2064d
skea <253590433@qq.com>
2021-07-06 13:47:52 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react'
+import { DatePicker, ConfigProvider } from 'antd'
+import dayjs from 'dayjs'
+import zhCN from 'antd/lib/locale/zh_CN'
+import 'dayjs/locale/zh-cn'
+import 'antd/dist/antd.less'
 import './index.less'
-import './test.less'
-import img from '../assets/image/smile.jpg'
-console.log(img)
+import img from '@/assets/image/smile.jpg'
+dayjs.locale('zh-cn')
 export default class App extends React.Component {
     render() {
-        return <div>
+        return <ConfigProvider locale={zhCN}><div>
+            <DatePicker />
             hello App
             <img src={img} alt="" />
-        </div>
+        </div></ConfigProvider>
     }
 }
