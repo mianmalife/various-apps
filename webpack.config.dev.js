7602c0b469632ae821bf86d122f5baf0f881ce49
skea <253590433@qq.com>
2021-07-06 13:46:43 +0800

    Update webpack.config.dev.js

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,61 +1,11 @@
 const path = require('path')
-const webpack = require('webpack')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-module.exports = {
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common.js')
+module.exports = merge(common, {
     mode: 'development',
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: '[name][hash:6].js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(css|less)$/,
-                use: ['style-loader', 'css-loader', 'postcss-loader', 'less-loader']
-            },
-            {
-                test: /\.(png|jpe?g|gif|svg)$/i,
-                loader: 'file-loader',
-                options: {
-                    name: '[name][hash:6].[ext]',
-                    outputPath: 'image'
-                }
-            },
-            {
-                test: /\.m?(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react'],
-                        plugins: ['@babel/plugin-transform-runtime']
-                    }
-                }
-            }
-        ]
-    },
-    resolveLoader: {
-        modules: ['node_modules', 'test-loaders']
-    },
     devtool: 'source-map',
     devServer: {
         contentBase: path.resolve(__dirname, 'dist'),
         hot: true
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: '首页',
-            template: 'public/index.html',
-            inject: true,
-            chunks: ['main']
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'css/index-[hash:6].css'
-        }),
-        new CleanWebpackPlugin(),
-        new webpack.HotModuleReplacementPlugin()
-    ]
-}
+    }
+})
